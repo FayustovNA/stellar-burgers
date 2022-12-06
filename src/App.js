d921b95e79386514b43150e6b024fe9551f36f62
Nikolay Fayustov <n.a.fayustov@gmail.com>
2022-12-06 21:04:51 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import styles from './components/mainorderbox/mainorderbox.module.css';
+import AppHeader from './components/appHeader/appHeader';
+import BurgerConstructor from './components/burgerConstructor/burgerConstructor';
+import BurgerIngredients from './components/burgerIngredients/burgerIngredients';
+
+const url = 'https://norma.nomoreparties.space/api/ingredients';
 
 function App() {
+  const [ingredients, setIngredients] = React.useState([]);
+
+  const getData = async () => {
+    const res = await fetch(url)
+    const data = await res.json()
+    setIngredients(data.data)
+  }
+
+  React.useEffect(() => {
+    getData();
+  }, [])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppHeader />
+      <section className={styles.mainorderbox}>
+        <BurgerIngredients ingredients={ingredients} />
+        <BurgerConstructor ingredients={ingredients} />
+      </section>
     </div>
   );
 }
