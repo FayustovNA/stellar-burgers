60620b0165eb9ff48b9a6bc6997bef776993ab4e
FayustovNA <106696873+FayustovNA@users.noreply.github.com>
2022-12-09 22:14:48 +0300

    Add files via upload

---
+++ src/components/cards-ingredients/cards-ingredients.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import styles from './cards-ingredients.module.css';
+import { CurrencyIcon, Counter } from '@ya.praktikum/react-developer-burger-ui-components';
+import PropTypes from 'prop-types';
+import { dataPropTypes } from '../../utils/type.js';
+
+function CardIngredient({ data, onClick }) {
+    const image = (
+        <img src={data.image} alt={data.name} />
+    );
+
+    return (
+        <section className={styles.maincontainer} onClick={() => onClick(data)}>
+            <span className={styles.counter}><Counter /></span>
+            <div className={styles.image}>{image}</div>
+            <p className={styles.price}>{data.price} <CurrencyIcon type="primary" /></p>
+            <h2 className={styles.title}>{data.name}</h2>
+        </section >
+    );
+}
+
+export default CardIngredient;
+
+CardIngredient.propTypes = {
+    data: PropTypes.shape({
+        _id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        type: PropTypes.string.isRequired,
+        proteins: PropTypes.number.isRequired,
+        fat: PropTypes.number.isRequired,
+        carbohydrates: PropTypes.number.isRequired,
+        calories: PropTypes.number.isRequired,
+        price: PropTypes.number.isRequired,
+        image: PropTypes.string.isRequired,
+        image_mobile: PropTypes.string.isRequired,
+        image_large: PropTypes.string.isRequired,
+        __v: PropTypes.number.isRequired,
+    }),
+    onClick: PropTypes.func
+}
