ebbb252323f3e6c215b59969c949b47b653358b9
Nikolay <106696873+FayustovNA@users.noreply.github.com>
2023-04-02 13:48:46 +0300

    Delete total-price.js

--- src/utils/total-price.js
+++
@@ -1,7 +0,0 @@
-
-// export const totalPricing = state => {
-//     const {
-//         maker: { otherIngredients, bun },
-//     } = state;
-//     return otherIngredients.reduce((acc, item) => acc + item.price, 0) + bun.reduce((acc, item) => acc + item.price * 2, 0);
-// };
